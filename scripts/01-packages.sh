c45ea76fe9e50e6a7d727c8f15e6da7e0ebc144a
Mauricio Vargas <mvargas@dcc.uchile.cl>
2021-02-28 04:08:37 -0300

    R 4.0 upgrade + using packer to create img

--- scripts/01-packages.sh
+++ scripts/01-packages.sh
@@ -6,8 +6,8 @@ mkswap /swapfile
 swapon /swapfile
 
 # add CRAN to apt sources
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-printf '\n#CRAN mirror\ndeb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/\n' | sudo tee -a /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+printf '\n#CRAN mirror\ndeb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/\n' | tee -a /etc/apt/sources.list
 
 # update apt
 apt-get -y update
@@ -16,93 +16,59 @@ apt-get -y upgrade
 # requisites for RStudio
 apt-get -y install gdebi-core
 
-# requisites for R packages
-apt-get -y install libxml2-dev libssl-dev libcurl4-openssl-dev libudunits2-dev gdal-bin libgdal-dev libproj-dev libv8-dev default-jdk libmagick++-dev
+# requisites for different R packages
+# this aims at covering as much packages as possible
+# see https://packagemanager.rstudio.com/client/#/repos/1/overview
+# and also
+# https://geocompr.github.io/post/2020/installing-r-spatial-ubuntu/
+# http://dirk.eddelbuettel.com/blog/2020/06/22#027_ubuntu_binaries
+# this a broad, general setup, no fine detail here
+apt-get -y install software-properties-common libopenblas-dev libsodium-dev texlive default-jdk
 
-# requisites for Markdown
-apt-get -y install texlive-full pandoc-citeproc
-
-# requisites for DB connectors
-apt-get -y install libmysqlclient-dev libpq-dev
-
-# install optimized BLAS
-apt-get -y install libopenblas-dev
+add-apt-repository -y ppa:c2d4u.team/c2d4u4.0+
+apt-get -y update
 
 # install R
 apt-get -y install r-base r-base-dev
+R CMD javareconf
+
+# RSPM site-wide
+sed -i 's/cloud\.r-project\.org/packagemanager\.rstudio\.com\/all\/latest/g' /usr/lib/R/etc/Rprofile.site
 
 # install Digital Ocean agent
 curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash
 
 # install R packages
-R --vanilla << EOF
-# development
-if (!require("pacman")) {
-  install.packages("pacman", repos = "http://cran.us.r-project.org")
-}
-q()
-EOF
+apt-get -y install r-cran-devtools r-cran-usethis r-cran-tidyverse r-cran-janitor r-cran-writexl r-cran-data.table r-cran-roxygen2 r-cran-knitr r-cran-rmarkdown r-cran-bookdown r-cran-xaringan r-cran-rdpack r-cran-dbi r-cran-dbplyr r-cran-rmariadb r-cran-rpostgres r-cran-glue r-cran-doparallel r-cran-future r-cran-shiny r-cran-shinyjs r-cran-shinydashboard r-cran-dt
 
 R --vanilla << EOF
-# development
-pacman::p_install(devtools, force = FALSE)
-pacman::p_install(testthat, force = FALSE)
-pacman::p_install(usethis, force = FALSE)
+options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/latest"))
 
-# general
-pacman::p_install(data.table, force = FALSE)
-pacman::p_install(tidyverse, force = FALSE)
-pacman::p_install(lubridate, force = FALSE)
-pacman::p_install(janitor, force = FALSE)
-pacman::p_install(haven, force = FALSE)
-pacman::p_install(readxl, force = FALSE)
-pacman::p_install(writexl, force = FALSE)
-pacman::p_install(here, force = FALSE)
-pacman::p_install(fs, force = FALSE)
-
-# documentation
-pacman::p_install(roxygen2, force = FALSE)
-pacman::p_install(rmarkdown, force = FALSE)
-pacman::p_install(bookdown, force = FALSE)
-pacman::p_install(pkgdown, force = FALSE)
-pacman::p_install(xaringan, force = FALSE)
-pacman::p_install(Rdpack, force = FALSE)
-
-# databases
-pacman::p_install(DBI, force = FALSE)
-pacman::p_install(dbplyr, force = FALSE)
-pacman::p_install(RMariaDB, force = FALSE)
-pacman::p_install(RPostgres, force = FALSE)
-pacman::p_install(glue, force = FALSE)
-pacman::p_install(chunked, force = FALSE)
-
-# paralelization
-pacman::p_install(doParallel, force = FALSE)
-
-# statistics
-pacman::p_install(broom, force = FALSE)
-pacman::p_install(modelr, force = FALSE)
-pacman::p_install(tidymodels, force = FALSE)
-
-# visualization
-pacman::p_install(shiny, force = FALSE)
-pacman::p_install(shinyjs, force = FALSE)
-pacman::p_install(shinydashboard, force = FALSE)
-pacman::p_install(highcharter, force = FALSE)
-pacman::p_install(DT, force = FALSE)
+install.packages("tidymodels")
+install.packages("pkgdown")
+install.packages("highcharter")
 
 q()
 EOF
 
 # install RStudio Server
-wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.2.5033-amd64.deb
-gdebi --n rstudio-server-1.2.5033-amd64.deb
-rm rstudio-server-1.2.5033-amd64.deb
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.4.1106-amd64.deb
+gdebi --n rstudio-server-1.4.1106-amd64.deb
+rm rstudio-server-1.4.1106-amd64.deb
 
 # install Shiny Server
-wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.12.933-amd64.deb
-gdebi --n shiny-server-1.5.12.933-amd64.deb
-rm shiny-server-1.5.12.933-amd64.deb
+wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.16.958-amd64.deb
+gdebi --n shiny-server-1.5.16.958-amd64.deb
+rm shiny-server-1.5.16.958-amd64.deb
+
+# add some security
+echo "y" | ufw enable
+apt-get -y install fail2ban
+systemctl start fail2ban
+systemctl enable fail2ban
+printf '[sshd]\nenabled = true\nport = 22\nfilter = sshd\nlogpath = /var/log/auth.log\nmaxretry = 5' | tee -a /etc/fail2ban/jail.local
+printf '\n\n[http-auth]\nenabled = true\nport = http,https\nlogpath = /var/log/auth.log\nmaxretry = 5' | tee -a /etc/fail2ban/jail.local
+systemctl restart fail2ban
 
 # open ports
 ufw allow http
